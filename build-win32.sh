ee50d07d63c483759bd0934217b94d0f4a705f55
MashedD <sl8code@gmail.com>
2026-03-27 16:54:04 +0100

    Fix .dll permissions

--- build-win32.sh
+++ build-win32.sh
@@ -2,4 +2,5 @@
 set -Eeuo pipefail
 
 make CONFIG_FILE=.config-win32 -j$(nproc)
+chmod -x gamex86.dll
 
--- build-win64.sh
+++ build-win64.sh
@@ -2,4 +2,5 @@
 set -Eeuo pipefail
 
 make CONFIG_FILE=.config-win64 -j$(nproc)
+chmod -x gamex86_64.dll
 
